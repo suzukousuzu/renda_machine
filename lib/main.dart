72d7f13def40d89864c3dfc18351d407cc3692a7
suzukikouta <addressuzuki@gmail.com>
2021-07-05 11:35:28 +0900

    debugの削除

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "連打マシーン",
       theme: ThemeData(fontFamily: "SourceSansPro"),
       home: HomeScreen(),
